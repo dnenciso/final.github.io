d147bb5598529d31a49e9f9bc4721005172f874f
Daniel Nicolas Enciso <denciso@URBANIZADORA.local>
2021-05-01 14:54:14 -0500

    Updates

---
+++ precache-manifest.376dcc1846b9db2a9715f078e4916a6e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3230bac041ea479645f9edcf329a26c7",
+    "url": "/dnenciso/final.github.io.git/index.html"
+  },
+  {
+    "revision": "afbc76b28247108e485f",
+    "url": "/dnenciso/final.github.io.git/static/css/main.242813e6.chunk.css"
+  },
+  {
+    "revision": "f235bd0a839c8b72f254",
+    "url": "/dnenciso/final.github.io.git/static/js/2.2cc7ef6e.chunk.js"
+  },
+  {
+    "revision": "de1cedeacf4641d6277b42334cfacb1d",
+    "url": "/dnenciso/final.github.io.git/static/js/2.2cc7ef6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afbc76b28247108e485f",
+    "url": "/dnenciso/final.github.io.git/static/js/main.136c0232.chunk.js"
+  },
+  {
+    "revision": "823cee7c86df0609f13b",
+    "url": "/dnenciso/final.github.io.git/static/js/runtime-main.51c21ad5.js"
+  },
+  {
+    "revision": "ac7d30f522cc223eea0b05e982e0c5bc",
+    "url": "/dnenciso/final.github.io.git/static/media/fondo.ac7d30f5.jpg"
+  }
+]);
